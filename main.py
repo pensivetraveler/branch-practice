e00fa8df80d36cc8baf6bb90246f05167564899e
pensivetraveler <pensivetraveler@gmail.com>
2024-01-15 11:39:10 +0900

    refactor: Get numbers using randint()

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from random import sample
+from random import randint
 
 def get_luckies() -> list:
-	return sample(range(1,45+1), k=6)
+    # _ 라는 건 ignore, for 문에서 내부 변수를 사용하지 않고 있으므로 ignore를 사용한 것
+    return [randint(1,45) for _ in range(6)]
 
 if __name__ == '__main__':
-	print(get_luckies())
+    print(get_luckies())
