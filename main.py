8708b4fc653c48b799abd3c91eb5dac7e4cd7d66
pensivetraveler <pensivetraveler@gmail.com>
2024-01-15 11:22:59 +0900

    docs: add annotation of '_'

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from random import randint
 
 def get_luckies() -> list:
+	# _ 라는 건 ignore, for 문에서 내부 변수를 사용하지 않고 있으므로 ignore를 사용한 것
 	return [randint(1,45) for _ in range(6)]
 
 if __name__ == '__main__':
